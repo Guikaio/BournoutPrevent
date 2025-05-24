5bf7dbc9132c46ae0634bfe469c0e0f4b4170963
GuiKaio <Guilhermekaio10@gmail.com>
2025-05-24 20:51:33 -0300

    Primeiro commit para deploy Heroku

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+import os
 from create_app import create_app
 
-# Criar a instância da aplicação
 app = create_app()
 
-# Necessário para o Gunicorn
-application = app
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-    
+    # pegar a porta da variável ou usar 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
